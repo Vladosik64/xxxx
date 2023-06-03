c249648bdbdbc29e25606a6b0ec123c6fbd5162f
Vladosik64 <apexes_boils.0r@icloud.com>
2023-06-03 18:06:30 +0300

    Initial Commit

---
+++ xxxx/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  xxxx
+//
+//  Created by Vlados64 on 03.06.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
